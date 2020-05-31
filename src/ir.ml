da758770a41e05680c1f0583169133551fe0fe15
Zach Sisco <zach@zsisco.net>
2020-05-30 17:07:40 -0700

    first pass at apply milnerization

--- src/ir.ml
+++ src/ir.ml
@@ -4,6 +4,14 @@ open Cenv
 
 exception NonMilnerizableError
 
+(* for generating anonymous function names *) 
+let next_l : int64 ref = ref Int64.zero
+let fresh_l () : int64 = begin
+    next_l := Int64.add (! next_l) Int64.one;
+    ! next_l
+    end
+
+
 let rec milnerize (e: env) (expr: cexp) : (env * cexp) =       
   match expr with
   (*
@@ -34,7 +42,7 @@ let rec milnerize (e: env) (expr: cexp) : (env * cexp) =
                   Atom("infinity"), Atom("true"))
           in
           (e', Fun(name, arity, [], recv))
-  | Apply(fname, arity, arg) ->
+  | Apply(fname, arity, args) ->
           (* [[MN]]a := νr [[M]]b[[N]]cb_cr | r(a).[] *)
           (* in core erlang:
            * let <Lhs> = spawn fname in
@@ -42,7 +50,42 @@ let rec milnerize (e: env) (expr: cexp) : (env * cexp) =
            * let Rhs = spawn Recv in 
            * call ! (Lhs, Tuple(arg, Rhs))
            *)
-          e, Atom("ok")
+          let arg = match args with
+                    | h::t -> h
+                    | [] -> raise NonMilnerizableError (* can't apply with empty argument *)
+          in
+          let lhs_var = "Lhs_" ^ fname
+          in
+          let recv_var = "_recv@" ^ (Int64.to_string (fresh_l()))
+          in
+          let rhs_var = "Rhs_" ^ fname
+          in
+          let rhs = 
+              Let(
+                  Values([Var(rhs_var)]),
+                  Call("erlang", "spawn", [Var(recv_var)]),
+                  Call("erlang", "!", [Var(lhs_var); Tuple([arg; Var(rhs_var)])]))
+          in 
+          let recv = 
+              Let(
+                  Values([Var(recv_var)]),
+                  Fun("", 0, [],
+                    Receive(
+                      [Clause(Values([Var("X")]),
+                        Atom("true"),
+                        Var("X")
+                      )], 
+                      (* Receive boilerplate for timeout *)
+                      Atom("infinity"), Atom("true"))),
+                  rhs)
+          in 
+          let lhs = 
+              Let(
+                  Values([Var(lhs_var)]),
+                  Call("erlang", "spawn", [Atom(fname)]),
+                  recv)
+          in
+          e, lhs
 
   | _ -> raise NonMilnerizableError
 (*
--- src/ir_test.ml
+++ src/ir_test.ml
@@ -7,3 +7,7 @@ let () =
     print_endline "Milnerize identity function:";
     print_endline (start_gen_cerl m);
 
+    let e, m = milnerize (Cenv.make_env None) (Apply("foo", 0, [Atom("hey!")]))
+    in
+    print_endline "Milnerize apply:";
+    print_endline (start_gen_cerl m);
